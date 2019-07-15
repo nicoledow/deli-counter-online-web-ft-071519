adcaf738dc1b73efe3acd7975c3626e5b0fb4c2c
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 17:51:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,13 @@
 def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  end
   
+  if array.size > 0
+    sentence = "The line is currently: "
+    array.each_with_index do |person, i|
+      sentence << "#{i+1}. #{name} "
+      puts sentence
+    end
+  end
 end
