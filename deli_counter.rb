a22babfc7ab59a2d522b37275b308aed369ddd12
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 18:08:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(line_array)
   if line_array.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts
+    puts "Currently serving #{line_array[0].}"
     
   end
     
