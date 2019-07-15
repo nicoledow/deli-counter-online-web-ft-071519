91eb421dd843098125bed2524a82799564710532
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 18:02:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,17 @@
-def line(array)
-  if array.size == 0
+def line(line_array)
+  if line_array.size == 0
     puts "The line is currently empty."
   end
   
-  if array.size > 0
+  if line_array.size > 0
     sentence = "The line is currently:"
-    array.each_with_index do |person, i|
+    line_array.each_with_index do |person, i|
       sentence << " #{i+1}. #{person}"
     end
      puts sentence
   end
+end
+
+def take_a_number(line_array, person)
+  line_array << person
 end
